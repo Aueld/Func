899fc3c741af4c8a9fd98a1a9c925e2240570813
Aueld <44721590+Aueld@users.noreply.github.com>
2022-02-15 14:32:16 +0900

    Unity C#

---
+++ Unity Design Patterns/GameManagerSingleton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerSingleton : MonoBehaviour
+{
+    private static GameManagerSingleton instance = null;
+
+    void Awake()
+    {
+        if (null == instance)
+        {
+            instance = this;
+
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public static GameManagerSingleton Instance
+    {
+        get
+        {
+            if (null == instance)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+}
